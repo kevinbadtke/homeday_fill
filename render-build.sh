6be263b986d72e77306f6f2e4f590ec66451359d
kevinbadtke <k.badtke@tembo-consulting.de>
2024-10-24 17:08:09 +0800

    Create render-build.sh

---
+++ render-build.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# Install Chromium for Puppeteer
+apt-get update && apt-get install -y wget gnupg
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | apt-key add -
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list
+apt-get update
+apt-get install -y google-chrome-stable
+
+# Continue with the Node.js build
+npm install
